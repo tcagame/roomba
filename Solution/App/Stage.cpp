c3f4a478a2fa56b2227cbcca5bb7415ddb2e7ef4
ito <i>
2017-03-23 14:58:23 +0900

    ステージをワイヤーフレームで表示しました

--- Solution/App/Stage.cpp
+++ Solution/App/Stage.cpp
@@ -7,46 +7,65 @@ static const Vector MODEL_SIZE( 6, 6 );
 static const std::string STAGE_FILE_NAME = "../Resource/Model/Stage/floor01.mdl";
 static const Vector CRYSTAL_SCALE( 2, 2, 2 );
 
-Stage::Stage( ) :
-_width( 0 ),
-_height( 0 ) {
-	DrawerPtr drawer = Drawer::getTask( );
-	drawer->loadMDLModel( MDL::MDL_STAGE, "Model/Stage/floor01.mdl", "Model/Stage/floor01_DM.jpg" );
-	drawer->loadMDLModel( MDL::MDL_OBJ, "Model/Stage/floor01.mdl", "Model/Stage/floor01_DM.jpg");
-	Matrix mat = Matrix( );
-	mat = mat.makeTransformScaling( CRYSTAL_SCALE );
-	drawer->loadMDLModel( MDL::MDL_CRYSTAL, "Model/Crystal/crystal.mdl","Model/Crystal/crystal.jpg", mat );
-	_crystals.push_back( CrystalPtr( new Crystal( Vector( 5, 5, 1 ) ) ) );
-	loadMapCSV( );
-	loadObjCSV( );
-	loadModelPos( );
+Stage::Stage( ) {
+	_stage_data = { 
+		0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+	};
 }
 
 Stage::~Stage( ) {
 }
 
 void Stage::draw( ) {
-	DrawerPtr drawer = Drawer::getTask( );
-	for ( int i = 0; i < _width * _height; i++ ) {
-		MDL type = (MDL)_map_data[ i ];
-		if ( type == MDL::MDL_NONE ) {
-			continue;
-		}
-		if ( type == MDL::MDL_STAGE) {
-			Vector pos( i % _width * MODEL_SIZE.x, i / _height * MODEL_SIZE.y );
-			Drawer::ModelMDL model = Drawer::ModelMDL( pos, MDL::MDL_STAGE );
-			drawer->setModelMDL(model);
-		}
-		if ( type == MDL::MDL_OBJ ) {
-			Vector pos( i % _width * MODEL_SIZE.x, i / _height * MODEL_SIZE.y, 3 );
-			Drawer::ModelMDL model = Drawer::ModelMDL(pos, MDL::MDL_OBJ);
-			drawer->setModelMDL( model );
-		}	
-	}
+	drawWireFrame( );
 	drawCrystal( );
 }
 
+void Stage::drawWireFrame( ) {
+	const int pitch = 10;
+	DrawerPtr drawer = Drawer::getTask( );
 
+	//　床の描画
+	for ( int i = 0; i <= 10; i++ ) {
+		Vector vec1 = Vector( 0, 0, 0 );
+		Vector vec2 = Vector( 0, 100, 0 );
+		Vector vec3 = Vector( 100, 0, 0 );
+		Vector pitchX = Vector( ( i * pitch ), 0, 0 );
+		Vector pitchY = Vector( 0, ( i * pitch ), 0 );
+		drawer->drawLine( vec1 + pitchX, vec2 + pitchX );
+		drawer->drawLine( vec1 + pitchY, vec3 + pitchY );
+	}
+
+	// 立体の描画
+	for ( int i = 0; i < STAGE_SIZE; i++ ) {
+		if ( _stage_data[ i ] == 1 ) {
+			Vector vec1 = Vector( ( i % 10 ) * pitch, ( i / 10 ) * pitch, 0 );
+			Vector vec2 = vec1 + Vector( pitch, 0, 0 );
+			Vector vec3 = vec1 + Vector( 0, pitch, 0 );
+			Vector vec4 = vec1 + Vector( pitch, pitch, 0 );
+
+			//縦の辺
+			drawer->drawLine( vec1, vec1 + Vector( 0, 0, pitch ) );
+			drawer->drawLine( vec2, vec2 + Vector( 0, 0, pitch ) );
+			drawer->drawLine( vec3, vec3 + Vector( 0, 0, pitch ) );
+			drawer->drawLine( vec4, vec4 + Vector( 0, 0, pitch ) );
+			//上面
+			drawer->drawLine( vec1 + Vector( 0, 0, pitch ), vec2 + Vector( 0, 0, pitch ) );
+			drawer->drawLine( vec2 + Vector( 0, 0, pitch ), vec4 + Vector( 0, 0, pitch ) );
+			drawer->drawLine( vec3 + Vector( 0, 0, pitch ), vec4 + Vector( 0, 0, pitch ) );
+			drawer->drawLine( vec3 + Vector( 0, 0, pitch ), vec1 + Vector( 0, 0, pitch ) );
+		}
+	}
+}
 
 void Stage::drawCrystal( ) const {
 	std::list< CrystalPtr >::const_iterator ite = _crystals.begin( );
@@ -58,194 +77,4 @@ void Stage::drawCrystal( ) const {
 		(*ite)->draw( );
 		ite++;
 	}
-}
-
-void Stage::loadMapCSV( ) {
-	//ファイルの読み込み
-	const char* file_name = "../Resource/CSV/map.csv";
-	FILE* fp;
-	errno_t err = fopen_s( &fp, file_name, "r" );
-	if ( err != 0 ) {
-		return;
-	}
-	
-	char buf[ 2048 ];
-
-    //　カウントする
-	while ( fgets( buf, 2048, fp ) != NULL ) {
-		int width = 0;
-		std::string str = buf;
-		while ( true ) {
-			width++;
-			std::string::size_type index = str.find( "," );
-
-			if ( index == std::string::npos ) {
-				//","が見つからない場合
-				_map_data.push_back( atoi( str.c_str( ) ) );
-				break;
-			}
-			std::string substr = str.substr( 0, index );
-			_map_data.push_back( atoi( substr.c_str( ) ) );
-			str = str.substr( index + 1 );
-		}
-
-		if ( _height == 0 ) {
-			_width = width;
-		}
-		_height++;
-	}
-	return;
-}
-
-void Stage::loadObjCSV( ) {
-	//ファイルの読み込み
-	const char* file_name = "../Resource/CSV/obj.csv";
-	FILE* fp;
-	errno_t err = fopen_s( &fp, file_name, "r" );
-	if ( err != 0 ) {
-		return;
-	}
-
-	char buf[2048];
-
-	//　カウントする
-	while ( fgets( buf, 2048, fp ) != NULL ) {
-		int width = 0;
-		std::string str = buf;
-		while ( true ) {
-			width++;
-			std::string::size_type index = str.find( "," );
-
-			if ( index == std::string::npos ) {
-				//","が見つからない場合
-				_map_data.push_back(atoi(str.c_str()));
-				break;
-			}
-			std::string substr = str.substr(0, index);
-			_map_data.push_back(atoi(substr.c_str()));
-			str = str.substr(index + 1);
-		}
-
-		if (_height == 0) {
-			_width = width;
-		}
-		_height++;
-	}
-	return;
-}
-
-void Stage::loadModelPos( ) {
-	ModelPtr model( new Model );
-	model->load( STAGE_FILE_NAME );
-	int polygon_num = model->getPolygonNum( );
-
-	for ( int i = 0; i < _width * _height; i++ ) {
-		if ( _map_data[ i ] == 0 ) {
-			_model_data[ i ] = ModelData( );
-			continue;
-		}
-		Vector move( i % _width * MODEL_SIZE.x, i / _height * MODEL_SIZE.y );
-		_model_data[ i ].polygon_num = polygon_num;
-		model->translate( move );
-
-		double max_x = 0;
-		double max_y = 0;
-		double min_x = 10000;
-		double min_y = 10000;
-		for ( int j = 0; j < polygon_num * 3; j++ ) {
-			Vector pos = model->getPoint( j );
-			_model_data[ i ].pos[ j ] = pos;
-			if ( pos.x > max_x ) {
-				max_x = pos.x;
-			}
-			if ( pos.y > max_y ) {
-				max_y = pos.y;
-			}
-			if ( pos.x < min_x ) {
-				min_x = pos.x;
-			}
-			if ( pos.y < min_y ) {
-				min_y = pos.y;
-			}
-		}
-		_model_data[ i ].max_x = max_x;
-		_model_data[ i ].max_y = max_y;
-		_model_data[ i ].min_x = min_x;
-		_model_data[ i ].min_y = min_y;
-	}
-}
-
-bool Stage::isCollision( Vector pos, Vector vec, Vector scale ) {
-	bool ret = false;
-	Vector check_pos = pos + vec;
-
-	for ( int i = 0; i < _width * _height; i++ ) {
-		if ( _map_data[ i ] == 0 ) {
-			continue;
-		}
-		//モデルがある範囲にposが在るか確かめる
-		if ( _model_data[ i ].max_x < check_pos.x - scale.x ) {
-			continue;
-		}  
-		if ( _model_data[ i ].max_y < check_pos.y - scale.y ) {
-			continue;
-		}  
-		if ( _model_data[ i ].min_x > check_pos.x + scale.x ) {
-			continue;
-		}  
-		if ( _model_data[ i ].min_y > check_pos.y + scale.y ) {
-			continue;
-		}
-		//あたっているか確かめる
-		if ( vec.z > 0 ) {
-			check_pos.z += scale.z;
-		}
-		if ( vec.z < 0 ) {
-			check_pos.z -= scale.z;
-		}
-		if ( vec.x > 0 ) {
-			check_pos.x += scale.x;
-		}
-		if ( vec.x < 0 ) {
-			check_pos.x -= scale.x;
-		}
-		if ( vec.y > 0 ) {
-			check_pos.y += scale.y;
-		}
-		if ( vec.y < 0 ) {
-			check_pos.y -= scale.y;
-		}
-		ret = isCollisionModel( _model_data[ i ], check_pos );
-		if ( ret ) {
-			return true;
-		}
-	}
-	return false;
-}
-
-bool Stage::isCollisionModel( ModelData model, Vector check_pos ) {
-	Vector pos_p = check_pos;
-	int polygon_idx = 0;
-	for ( int i = 0; i < model.polygon_num; i++ ) {
-		polygon_idx = i * 3;
-		Vector pos_a = model.pos[ polygon_idx ];
-		Vector pos_b = model.pos[ polygon_idx + 1 ];
-		Vector pos_c = model.pos[ polygon_idx + 2 ];
-
-		Vector ab = pos_a.sub( pos_b );
-		Vector bp = pos_b.sub( pos_p );
-		Vector bc = pos_b.sub( pos_c );
-		Vector cp = pos_c.sub( pos_p );
-		Vector ca = pos_c.sub( pos_a );
-		Vector ap = pos_a.sub( pos_p );
-		Vector c1 = ab.cross( bp );
-		Vector c2 = bc.cross( cp );
-		Vector c3 = ca.cross( ap );
-		double dot1 = c1.dot( c2 );
-		double dot2 = c1.dot( c3 );
-		if ( dot1 > 0 && dot2 > 0 ) {
-			return true;
-		}
-	}
-	return false;
 }
--- Solution/App/Stage.h
+++ Solution/App/Stage.h
@@ -11,37 +11,15 @@ public:
 	virtual ~Stage( );
 public:
 	void draw( );
-	bool isCollision( Vector pos, Vector vec, Vector scale );
 private:
-	static const int MAX_POLYGON_NUM = 4096;
-	static const int MAX_MODEL_NUM = 1024;
+	static const int STAGE_SIZE = 110;
 private:
-	struct ModelData {
-		std::array< Vector, MAX_POLYGON_NUM > pos;
-		int polygon_num;
-		double max_x;
-		double max_y;
-		double min_x; 
-		double min_y;
-		ModelData( ) {
-			this->polygon_num = 0;
-			this->max_x = 0;
-			this->max_y = 0;
-			this->min_x = 0;
-			this->min_y = 0;
-		};
-	};
-private:
-	void loadMapCSV( );
-	void loadModelPos( );
-	void loadObjCSV( );
-	bool isCollisionModel( ModelData model, Vector check_pos );
 	void drawCrystal( ) const;
+	void drawWireFrame( );
 private:
 	std::vector< int > _map_data;
-	std::array< ModelData, MAX_MODEL_NUM > _model_data;
 	std::list< CrystalPtr > _crystals;
-	int _width;
-	int _height;
+	std::array< int, STAGE_SIZE > _stage_data;
+
 };
 
