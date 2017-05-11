89dab5e91d3fc29b4d8f3310b222a40348b43aeb
kase <kasu>
2017-05-11 12:09:12 +0900

    タイトル画面追加

--- Solution/App/Game.cpp
+++ Solution/App/Game.cpp
@@ -2,13 +2,13 @@
 #include "Application.h"
 #include "Scene.h"
 #include "SceneTitle.h"
+#include "SceneStage.h"
 #include "Drawer.h"
 #include "AppCamera.h"
 #include "Roomba.h"
 #include "Stage.h"
 #include "Timer.h"
 #include "define.h"
-#include "Device.h"
 
 static const double STAGE_MODEL_SIZE = 4;
 
@@ -18,9 +18,7 @@ GamePtr Game::getTask( ) {
 }
 
 Game::Game( ) :
-	_state( STATE_NORMAL ),
-	_select( 0 ),
-	_next( Scene::NEXT_TITLE ) {
+_next( Scene::NEXT_TITLE ) {
 	
 }
 
@@ -76,77 +74,73 @@ void Game::initialize( ) {
 	drawer->loadMDLModel( MDL_WALL_1_13, "Model/Stage/1_13.mdl", "Model/Stage/wall.jpg", size );
 	drawer->loadMDLModel( MDL_WALL_1_14, "Model/Stage/1_14.mdl", "Model/Stage/wall.jpg", size );
 	drawer->loadMDLModel( MDL_WALL_1_15, "Model/Stage/1_15.mdl", "Model/Stage/wall.jpg", size );
+	
+	changeScene( );
 
-
-	_stage = StagePtr( new Stage );
-	_roomba = RoombaPtr( new Roomba );
-	_camera = AppCameraPtr( new AppCamera( _roomba ) );
-	_timer = TimerPtr( new Timer );
 }
 
 void Game::update( ) {
-	//_next = _scene->update( );
+	_next = _scene->update( );
 	changeScene( );
+	/*
+	switch ( _next ) {
+	case Scene::NEXT_TITLE:
+		break;
+	case Scene::NEXT_STAGE:
+		if ( _state == STATE_NORMAL ) {
+			_roomba->update( _stage, _camera, _timer );
+			_stage->update( );
+			_camera->update( );
+			_timer->update( );
 
-	if ( _state == STATE_NORMAL ) {
-		_roomba->update( _stage, _camera, _timer );
-		_stage->update( );
-		_camera->update( );
-		_timer->update( );
-
-		if ( _timer->isTimeOver( ) ) {
-			_state = STATE_SELECT_RETRY;
+			if ( _timer->isTimeOver( ) ) {
+				_state = STATE_SELECT_RETRY;
+			}
+			if ( _stage->isFinished( ) ) {
+				_state = STATE_GAME_CLEAR;
+			}
 		}
-		if ( _stage->isFinished( ) ) {
-			_state = STATE_GAME_CLEAR;
+		if ( _state == STATE_SELECT_RETRY ) {
+			//選択したらリセット
+			DevicePtr device = Device::getTask( );
+			if ( device->getDirY( ) > 0 ) {
+				_select = 1;
+			}
+			if ( device->getDirY( ) < 0 ) {
+				_select = 0;
+			}
+			if ( device->getButton( ) & BUTTON_D ) {
+				_roomba->reset( );
+				_stage->reset( );
+				_camera->reset( );
+				_timer->reset( );
+				_state = STATE_NORMAL;
+			}
 		}
+		_stage->draw( );
+		_roomba->draw( );
+		_timer->draw( );
+		drawResult( );
+		break;
 	}
-	if ( _state == STATE_SELECT_RETRY ) {
-		//選択したらリセット
-		DevicePtr device = Device::getTask( );
-		if ( device->getDirY( ) > 0 ) {
-			_select = 1;
-		}
-		if ( device->getDirY( ) < 0 ) {
-			_select = 0;
-		}
-		if ( device->getButton( ) & BUTTON_D ) {
-			_roomba->reset( );
-			_stage->reset( );
-			_camera->reset( );
-			_timer->reset( );
-			_state = STATE_NORMAL;
-		}
-	}
-	_stage->draw( );
-	_roomba->draw( );
-	_timer->draw( );
-	drawResult( );
+	*/
 }
 
-void Game::drawResult( ) {
-	if ( _state == STATE_GAME_CLEAR ) {
-		DrawerPtr drawer = Drawer::getTask( );
-		drawer->drawString( 700, 400, "げーむくりあ" );
-	}
-	if ( _state == STATE_SELECT_RETRY ) {
-		//選択画面
-		DrawerPtr drawer = Drawer::getTask( );
-		ApplicationPtr app = Application::getInstance( );
-		int width = app->getWindowWidth( );
-		int height = app->getWindowHeight( );
-		int select_y = height / 2 + 32 + _select * 64;
-		drawer->setSprite( Drawer::Sprite( Drawer::Transform( width / 2 - 128, height / 2 - 128, 0, 0, 256, 256 ), GRAPH_SELECT_MENU ) );
-		drawer->setSprite( Drawer::Sprite( Drawer::Transform( width / 2 - 64, height / 2, 256, 0, 128, 128 ), GRAPH_SELECT_MENU ) );
-		drawer->setSprite( Drawer::Sprite( Drawer::Transform( width / 2 - 64, height / 2 + 64, 256, 0, 128, 128 ), GRAPH_SELECT_MENU ) );
-		drawer->setSprite( Drawer::Sprite( Drawer::Transform( width / 2 - 128, select_y, 384, 0, 64, 64 ), GRAPH_SELECT_MENU ) );
-	}
-}
+
 
 void Game::changeScene( ) {
+	if ( _next == Scene::NEXT_CONTINUE ) {
+		return;
+	}
+
+	_scene.reset( );
+
 	switch ( _next ) {
 	case Scene::NEXT_TITLE:
 		_scene = ScenePtr( new SceneTitle );
+		break;
+	case Scene::NEXT_STAGE:
+		_scene = ScenePtr( new SceneStage );
 		break;
 	}
 }
--- Solution/App/SceneStage.h
+++ Solution/App/SceneStage.h
@@ -1,9 +1,29 @@
 #pragma once
 #include "Scene.h"
+#include "smart_ptr.h"
+
+PTR( Roomba );
+PTR( AppCamera );
+PTR( Stage );
+PTR( Timer );
 
 class SceneStage : public Scene {//ステージ
 public:
 	SceneStage( );
 	virtual ~SceneStage( );
+	Scene::NEXT update( );
+private:
+	enum STATE {
+		STATE_NORMAL,
+		STATE_GAME_CLEAR,
+		STATE_SELECT_RETRY,
+		MAX_STATE,
+	};
+private:
+	AppCameraPtr _camera;
+	StagePtr _stage;
+	RoombaPtr _roomba;
+	TimerPtr _timer;
+	STATE _state;
 };
 
