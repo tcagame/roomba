8549525da55b60a18f1dd1b1cc1daa88b2cd6155
kiyotaka <kytk>
2017-05-19 09:42:55 +0900

    タイマー強調表示

--- Solution/App/Timer.cpp
+++ Solution/App/Timer.cpp
@@ -3,7 +3,7 @@
 #include "define.h"
 
 static const int FPS = 60;
-static const int START_TIME = 3 * FPS;
+static const int START_TIME = 10 * FPS;
 static const int ADD_TIME = 1;
 
 Timer::Timer( ) :
@@ -24,34 +24,43 @@ void Timer::draw( ) const {
 	}
 
 	DrawerPtr drawer = Drawer::getTask( );
-	int x = 640;
+	int x = 720;
 	int y = 10;
 	const int TW = 32;
 	const int TH = 64;
 	const int u[ ] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	int picth = 0;
 	int time = _timer / FPS;
+	// 強調表示
+	bool big = false;
+	if ( time < 6 ) {
+		big = true;
+		x += TW;
+	}
+
+	// タイマー表示
 	{
-		Drawer::Sprite sprite( Drawer::Transform( x, y, u[ ( _timer % FPS ) / 6 ] * TW, 0, TW, TH, x + TW, y + TH ), GRAPH_TIMER_NUM );
+		// 小数点以下数字
+		Drawer::Sprite sprite( Drawer::Transform( x, y, u[ ( _timer % FPS ) / 6 ] * TW, 0, TW, TH, x + TW + TW * big, y + TH + TH * big ), GRAPH_TIMER_NUM );
 		drawer->setSprite( sprite );
 	}
 	{
-		picth += TW / 2;
-		Drawer::Sprite sprite( Drawer::Transform( x - picth, y, 10 * TW, 0, TW / 2, TH, x - picth + TW / 2, y + TH ), GRAPH_TIMER_NUM );
+		// 小数点
+		picth += TW / 2 + TW * big;
+		Drawer::Sprite sprite( Drawer::Transform( x - picth, y, 10 * TW, 0, TW / 2, TH, x - picth + TW / 2 + TW * big, y + TH + TH * big ), GRAPH_TIMER_NUM );
 		drawer->setSprite( sprite );
-		picth += TW;
+		picth += TW + TW * big;
 	}
-	while ( time >= 0 ) {
-		Drawer::Sprite sprite( Drawer::Transform( x - picth, y, u[ time % 10 ] * TW, 0, TW, TH, x - picth + TW, y + TH ), GRAPH_TIMER_NUM );
+	while ( time >= 0 ) { // 小数点以上数字
+		Drawer::Sprite sprite( Drawer::Transform( x - picth, y, u[ time % 10 ] * TW, 0, TW, TH, x - picth + TW + TW * big, y + TH + TH * big ), GRAPH_TIMER_NUM );
 		drawer->setSprite( sprite );
-		picth += TW;
+		picth += TW + TW * big;
 		time /= 10;
 		
 		if ( time == 0 ) {
 			break;
 		}
 	}
-
 }
 
 void Timer::addTime( ) {
