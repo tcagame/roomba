5296b91cd185be1d3363c75ca751573f06c6b64e
ito <i>
2017-06-30 10:37:38 +0900

    expert追加

--- Solution/App/SceneSelect.cpp
+++ Solution/App/SceneSelect.cpp
@@ -35,13 +35,13 @@ _open_stage( open_stage ) {
 	drawer->loadGraph( GRAPH_CIRCLE, "scene/circle_blue1.png" );
 	drawer->loadGraph( GRAPH_STAGE_SELECT, "select/Stage Select.png" );
 	drawer->loadGraph( GRAPH_SELECT, "select/Select.png" );
-	drawer->loadGraph( GRAPH_BEGINNER, "select/beginner_1.png" );
+	drawer->loadGraph( GRAPH_BEGINNER, "select/beginner.png" );
 	if ( _open_stage ) {
 		drawer->loadGraph( GRAPH_NORMAL, "select/normal.png" );
-		drawer->loadGraph( GRAPH_NUM_3, "select/3.png" );
+		drawer->loadGraph( GRAPH_PRO, "select/professional.png" );
 	} else {
 		drawer->loadGraph( GRAPH_NORMAL, "select/close.png" );
-		drawer->loadGraph( GRAPH_NUM_3, "select/close.png" );
+		drawer->loadGraph( GRAPH_PRO, "select/close.png" );
 	}
 	freazeSelect( );
 	resetCount( );
@@ -184,33 +184,33 @@ void SceneSelect::drawSelect( ) {
 	GRAPH graph = (GRAPH)( GRAPH_BEGINNER + select );
 	
 	//^‚ñ’†
-	if ( graph == (GRAPH)( GRAPH_NUM_3 + 1 ) ) {
+	if ( graph == (GRAPH)( GRAPH_PRO + 1 ) ) {
 		graph = GRAPH_BEGINNER;
 	}
 	if ( graph == (GRAPH)( GRAPH_BEGINNER - 1 ) ) {
-		graph = GRAPH_NUM_3;
+		graph = GRAPH_PRO;
 	}
 	Drawer::Sprite sprite_1( Drawer::Transform( (int)_pos[ 0 ].x, (int)_pos[ 0 ].y, 0, 0, NUM_SIZE, NUM_SIZE  ), graph );
 	drawer->setSprite( sprite_1 );
 
 	//¶
 	graph = (GRAPH)( graph + 1 );
-	if ( graph == (GRAPH)( GRAPH_NUM_3 + 1 ) ) {
+	if ( graph == (GRAPH)( GRAPH_PRO + 1 ) ) {
 		graph = GRAPH_BEGINNER;
 	}
 	if ( graph == (GRAPH)( GRAPH_BEGINNER - 1 ) ) {
-		graph = GRAPH_NUM_3;
+		graph = GRAPH_PRO;
 	}
 	Drawer::Sprite sprite_2( Drawer::Transform( (int)_pos[ 1 ].x, (int)_pos[ 1 ].y, 0, 0, NUM_SIZE, NUM_SIZE, (int)_pos[ 1 ].x + NUM_SIZE * 2 / 5, (int)_pos[ 1 ].y + NUM_SIZE * 2 / 5 ), graph );
 	drawer->setSprite( sprite_2 );
 
 	//‰E
 	graph = (GRAPH)( graph + 1 );
-	if ( graph == (GRAPH)( GRAPH_NUM_3 + 1 ) ) {
+	if ( graph == (GRAPH)( GRAPH_PRO + 1 ) ) {
 		graph = GRAPH_BEGINNER;
 	}
 	if ( graph == (GRAPH)( GRAPH_BEGINNER - 1 ) ) {
-		graph = GRAPH_NUM_3;
+		graph = GRAPH_PRO;
 	}
 	Drawer::Sprite sprite_3( Drawer::Transform( (int)_pos[ 2 ].x, (int)_pos[ 2 ].y, 0, 0, NUM_SIZE, NUM_SIZE, (int)_pos[ 2 ].x + NUM_SIZE * 2 / 5, (int)_pos[ 2 ].y + NUM_SIZE * 2 / 5 ), graph );
 	drawer->setSprite( sprite_3 );
